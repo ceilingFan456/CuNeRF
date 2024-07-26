5cba3de81d43667a17e969d6aab07a530869069b
Qiming <e0271228@u.nus.edu>
2024-07-26 16:46:41 +0800

    Add support for hash encoding.

--- src/models/NGPMLP.py
+++ src/models/NGPMLP.py
@@ -3,20 +3,22 @@ from torch import nn
 import torch.nn.functional as F
 from . import base
 
-class NeRFMLP(base.baseModel):
+import tinycudann as tcnn
+
+
+class NGPMLP(base.baseModel):
     def __init__(self, **params):
-        super(NeRFMLP, self).__init__(params)
+        super(NGPMLP, self).__init__(params)
         self.coords_MLP = nn.ModuleList(
             [nn.Linear(self.in_ch, self.netW), *[nn.Linear(self.netW + self.in_ch, self.netW) if i in self.skips else nn.Linear(self.netW, self.netW) for i in range(self.netD - 1)]]
         )
         self.out_MLP = nn.Linear(self.netW, self.out_ch)
-        self.freqs = 2. ** torch.linspace(0., self.max_freq, steps=self.max_freq + 1)
-        self.in_ch = self.in_ch * (len(self.p_fns) * (self.max_freq + 1) + 1)
-
-
+        self.encoding = tcnn.Encoding(self.in_ch, self.hash_encoding_parameters)
+    
+    ## override 
     def embed(self, coords):
-        return torch.cat([coords, *[getattr(torch, p_fn)(coords * freq) for freq in self.freqs for p_fn in self.p_fns]], -1)
-
+        return self.encoding(coords)
+        
     def forward(self, x):
         x = self.embed(x)
         h = x
--- src/models/base.py
+++ src/models/base.py
@@ -8,8 +8,8 @@ class baseModel(torch.nn.Module):
         super(baseModel, self).__init__()
         for k, v in params.items():
             setattr(self, k, v)
-        self.freqs = 2. ** torch.linspace(0., self.max_freq, steps=self.max_freq + 1)
-        self.in_ch = self.in_ch * (len(self.p_fns) * (self.max_freq + 1) + 1)
 
+    ## override this method
     def embed(self, coords):
-        return torch.cat([coords, *[getattr(torch, p_fn)(coords * freq) for freq in self.freqs for p_fn in self.p_fns]], -1)
+        pass
+
